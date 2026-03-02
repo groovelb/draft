6f66e23d6140bac8f988587b52d8baf3ebd53856
groovelb <groovelb@gmail.com>
2026-03-02 20:31:29 +0900

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 
 import { defaultTheme as theme } from './styles/themes';
-
-function HomePage() {
-  return (
-    <Box
-      sx={{
-        p: 4,
-        textAlign: 'center',
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}
-    >
-      <Typography variant="h3" gutterBottom>
-        Starter Kit
-      </Typography>
-      <Typography color="text.secondary">Your design system foundation</Typography>
-    </Box>
-  );
-}
+import { DraftLanding } from './components/templates/DraftLanding';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={ theme }>
       <CssBaseline />
       <BrowserRouter>
         <Routes>
-          <Route index element={<HomePage />} />
+          <Route index element={ <DraftLanding /> } />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
